57ba4f1954f8a68d8cb09a330c29300964b4dd74
takakuni <takakuni.haraguchi@gmail.com>
2021-06-16 11:13:33 +0900

    チャプター2まで

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -26,15 +26,15 @@ Route::get('/', function () {
 });
 
 /**
-* 本を追加
+* 本を登録
 */
 Route::post('/books', function (Request $request) {
     //バリデーション
     $validator = Validator::make($request->all(), [
-        'item_name' => 'required|max:255',
-        'item_number' => 'required|max:255',
-        'item_amount' => 'required|max:255',
-        'published' => 'required|max:255',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:3',
+        'item_amount' => 'required|max:6',
+        'published' => 'required|date',
     ]);
 
     //バリデーション:エラー 
@@ -47,9 +47,9 @@ Route::post('/books', function (Request $request) {
     // Eloquentモデル（登録処理）
     $books = new Book;
     $books->item_name = $request->item_name;
-    $books->item_number = '1';
-    $books->item_amount = '1000';
-    $books->published = '2017-03-07 00:00:00';
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
     $books->save(); 
     return redirect('/');
 });
@@ -59,6 +59,45 @@ Route::post('/books', function (Request $request) {
 */
 Route::delete('/book/{book}', function (Book $book) {
     $book->delete();
+    return redirect('/');
+});
+
+/**
+* 更新画面
+*/
+Route::post('/booksedit/{books}', function (Book $books) {
+    return view('booksedit', [
+        'book' => $books
+    ]);
+});
+
+/**
+* 本を更新
+*/
+Route::post('/books/update', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'id' => 'required',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:3',
+        'item_amount' => 'required|max:6',
+        'published' => 'required|date',
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // データ更新
+    $books = Book::find($request->id);
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save();
     return redirect('/');
 });
 
